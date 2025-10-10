f52803e65cde497b9badd287d136db72b8d28233
river <breazile.river@student.clackamas.edu>
2025-10-10 13:29:04 -0700

    creating data.js file for array of JS object of items for shopping list app

---
+++ data.js/data.js
@@ -0,0 +1,46 @@
+/**
+ * data.js
+ * 
+ * All of the data (listItems) information organized into an array
+ * of JavaScript objects
+ */
+
+const listItems = [
+    {   id: 1, 
+        itemName: 'dog flower dress', 
+        section: 'canine',
+        coupon: false 
+    },
+    {   id: 2, 
+        itemName: 'soft cat tuna treats', 
+        section: 'feline', 
+        coupon: true 
+    },
+    {   id: 3, 
+        itemName: 'small blue and yellow tennis ball', 
+        section: 'canine', 
+        coupon: true 
+    },
+    {   id: 4, 
+        itemName: 'small non-skid dog socks', 
+        section: 'canine', 
+        coupon: true 
+    },
+    {   id: 5, 
+        itemName: 'extra small running wheel', 
+        section: 'rodent', 
+        coupon: false 
+    },
+    {   id: 6, 
+        itemName: 'very yummy catnip', 
+        section: 'feline', 
+        coupon: true 
+    },
+    {   id: 7, 
+        itemName: 'tiny animal bathing sand', 
+        section: 'rodent', 
+        coupon: false 
+    }
+];
+
+module.exports = listItems;
