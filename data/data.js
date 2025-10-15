d6f05bf5013c9e756119a41abb5edcc3e72bbfcf
river <breazile.river@student.clackamas.edu>
2025-10-15 15:37:27 -0700

    created file with array of JavaScript objects named listItems with id, itemName, section, and desc

---
+++ data/data.js
@@ -0,0 +1,46 @@
+/**
+ * data.js
+ * 
+ * All of the data (listItems) information organized into an array
+ * of JavaScript objects
+ */
+
+const listItems = [
+    {   id: 1, 
+        itemName: 'Dog flower dress', 
+        section: 'canine',
+        desc: 'Cute white dress with sunflowers to make your dog the stylish one on the block!' 
+    },
+    {   id: 2, 
+        itemName: 'Soft cat tuna treats', 
+        section: 'feline', 
+        desc: 'It is true when they say cats love tuna so buy them these soft cat treats!' 
+    },
+    {   id: 3, 
+        itemName: 'Small blue and yellow tennis ball', 
+        section: 'canine', 
+        desc: 'Bouncy ball tp throw for your furry friend'
+    },
+    {   id: 4, 
+        itemName: 'Small non-skid dog socks', 
+        section: 'canine', 
+        desc: 'Small non-skid dog socks to help your older friend stay on their paws!' 
+    },
+    {   id: 5, 
+        itemName: 'Extra small running wheel', 
+        section: 'rodent', 
+        desc: 'Running wheel to help your tiny friend run out their energy.'
+    },
+    {   id: 6, 
+        itemName: 'Very yummy catnip', 
+        section: 'feline', 
+        desc: 'Prestine organic catnip for a furry friend who needs to relax.' 
+    },
+    {   id: 7, 
+        itemName: 'Tiny animal bathing sand', 
+        section: 'rodent', 
+        desc: '6 oz of bathing sand for your little friend who likes to burrow.' 
+    }
+];
+
+module.exports = listItems;
